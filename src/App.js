5f9a18f521523cce0ac720b891ce52d6c1530301
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-14 11:42:13 +0100

    component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
+import Form from "./Components/Form";
+import Todo from "./Components/Todo";
+
 function App() {
+  const [inputText, setInputText] = useState('')
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Todo App</h1>
+      <Form />
+      <Todo />
     </div>
   );
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Form = () => {
+    const handleInput = e => {
+        console.log(e.target.value);
+    }
+  return (
+    <form action="">
+        <input type="text" className='todo-input' onChange={handleInput}/>
+        <button className="todo-button" type='submit'>
+            <i className="fas fa-plus-square"></i>
+        </button>
+
+        <div className="select">
+            <select name="todos" className="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Unompleted</option>
+            </select>
+        </div>
+    </form>
+  )
+}
+
+export default Form
